541ed1273b5d4bd680340fea72b4ee3494c1bdeb
Petr Stepchenko <nitrinodev@gmail.com>
2014-07-30 11:42:16 +0400

    Possibility to choose CA certificate

--- airbrake.go
+++ airbrake.go
@@ -1,54 +1,57 @@
 package airbrake
 
 import (
-	"bytes"
-	"errors"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"reflect"
-	"runtime"
-	"sync"
-	"text/template"
+  "bytes"
+  "crypto/tls"
+  "crypto/x509"
+  "errors"
+  "io/ioutil"
+  "log"
+  "net/http"
+  "os"
+  "reflect"
+  "runtime"
+  "sync"
+  "text/template"
 )
 
 var (
-	ApiKey      = ""
-	Endpoint    = "https://api.airbrake.io/notifier_api/v2/notices"
-	Environment = "development"
-	Verbose     = false
+  ApiKey      = ""
+  CAcert      = ""
+  Endpoint    = "https://api.airbrake.io/notifier_api/v2/notices"
+  Environment = "development"
+  Verbose     = false
 
-	badResponse   = errors.New("Bad response")
-	apiKeyMissing = errors.New("Please set the airbrake.ApiKey before doing calls")
-	dunno         = []byte("???")
-	centerDot     = []byte("·")
-	dot           = []byte(".")
-	tmpl          = template.Must(template.New("error").Parse(source))
+  badResponse   = errors.New("Bad response")
+  apiKeyMissing = errors.New("Please set the airbrake.ApiKey before doing calls")
+  dunno         = []byte("???")
+  centerDot     = []byte("·")
+  dot           = []byte(".")
+  tmpl          = template.Must(template.New("error").Parse(source))
 )
 
 type Line struct {
-	Function string
-	File     string
-	Line     int
+  Function string
+  File     string
+  Line     int
 }
 
 // stack implements Stack, skipping N frames
 func stacktrace(skip int) (lines []Line) {
-	for i := skip; ; i++ {
-		pc, file, line, ok := runtime.Caller(i)
-		if !ok {
-			break
-		}
+  for i := skip; ; i++ {
+    pc, file, line, ok := runtime.Caller(i)
+    if !ok {
+      break
+    }
 
-		item := Line{string(function(pc)), string(file), line}
+    item := Line{string(function(pc)), string(file), line}
 
-		// ignore panic method
-		if item.Function != "panic" {
-			lines = append(lines, item)
-		}
-	}
-	return
+    // ignore panic method
+    if item.Function != "panic" {
+      lines = append(lines, item)
+    }
+  }
+  return
 }
 
 var channel chan map[string]interface{}
@@ -56,147 +59,167 @@ var once sync.Once
 
 // function returns, if possible, the name of the function containing the PC.
 func function(pc uintptr) []byte {
-	fn := runtime.FuncForPC(pc)
-	if fn == nil {
-		return dunno
-	}
-	name := []byte(fn.Name())
-	// The name includes the path name to the package, which is unnecessary
-	// since the file name is already included.  Plus, it has center dots.
-	// That is, we see
-	//  runtime/debug.*T·ptrmethod
-	// and want
-	//  *T.ptrmethod
-	if period := bytes.Index(name, dot); period >= 0 {
-		name = name[period+1:]
-	}
-	name = bytes.Replace(name, centerDot, dot, -1)
-	return name
+  fn := runtime.FuncForPC(pc)
+  if fn == nil {
+    return dunno
+  }
+  name := []byte(fn.Name())
+  // The name includes the path name to the package, which is unnecessary
+  // since the file name is already included.  Plus, it has center dots.
+  // That is, we see
+  //  runtime/debug.*T·ptrmethod
+  // and want
+  //  *T.ptrmethod
+  if period := bytes.Index(name, dot); period >= 0 {
+    name = name[period+1:]
+  }
+  name = bytes.Replace(name, centerDot, dot, -1)
+  return name
 }
 
 func initChannel() {
-	channel = make(chan map[string]interface{}, 100)
+  channel = make(chan map[string]interface{}, 100)
 
-	go func() {
-		for params := range channel {
-			post(params)
-		}
-	}()
+  go func() {
+    for params := range channel {
+      post(params)
+    }
+  }()
 }
 
 func post(params map[string]interface{}) {
-	buffer := bytes.NewBufferString("")
+  buffer := bytes.NewBufferString("")
 
-	if err := tmpl.Execute(buffer, params); err != nil {
-		log.Printf("Airbrake error: %s", err)
-		return
-	}
+  if err := tmpl.Execute(buffer, params); err != nil {
+    log.Printf("Airbrake error: %s", err)
+    return
+  }
 
-	if Verbose {
-		log.Printf("Airbrake payload for endpoint %s: %s", Endpoint, buffer)
-	}
+  if Verbose {
+    log.Printf("Airbrake payload for endpoint %s: %s", Endpoint, buffer)
+  }
 
-	response, err := http.Post(Endpoint, "text/xml", buffer)
-	if err != nil {
-		log.Printf("Airbrake error: %s", err)
-		return
-	}
+  if CASert != "" {
+    CA_Pool := x509.NewCertPool()
+    severCert, err := ioutil.ReadFile(CASert)
+    if err != nil {
+      log.Printf("Airbrake error: %s", err)
+      return
+    }
+    CA_Pool.AppendCertsFromPEM(severCert)
+    tr := &http.Transport{
+      TLSClientConfig: &tls.Config{RootCAs: CA_Pool},
+    }
+    client := &http.Client{Transport: tr}
+  }
+  else{
+    tr := &http.Transport{
+      TLSClientConfig: &tls.Config{},
+    }
+    client := &http.Client{Transport: tr}
+  }
 
-	if Verbose {
-		body, _ := ioutil.ReadAll(response.Body)
-		log.Printf("response: %s", body)
-	}
-	response.Body.Close()
+  response, err := client.Post(Endpoint, "text/xml", buffer)
+  if err != nil {
+    log.Printf("Airbrake error: %s", err)
+    return
+  }
 
-	if Verbose {
-		log.Printf("Airbrake post: %s status code: %d", params["Error"], response.StatusCode)
-	}
+  if Verbose {
+    body, _ := ioutil.ReadAll(response.Body)
+    log.Printf("response: %s", body)
+  }
+  response.Body.Close()
+
+  if Verbose {
+    log.Printf("Airbrake post: %s status code: %d", params["Error"], response.StatusCode)
+  }
 
 }
 
 func Error(e error, request *http.Request) error {
-	once.Do(initChannel)
+  once.Do(initChannel)
 
-	if ApiKey == "" {
-		return apiKeyMissing
-	}
+  if ApiKey == "" {
+    return apiKeyMissing
+  }
 
-	params := map[string]interface{}{
-		"Class":       reflect.TypeOf(e).String(),
-		"Error":       e,
-		"ApiKey":      ApiKey,
-		"ErrorName":   e.Error(),
-		"Environment": Environment,
-		"Request":     request,
-	}
+  params := map[string]interface{}{
+    "Class":       reflect.TypeOf(e).String(),
+    "Error":       e,
+    "ApiKey":      ApiKey,
+    "ErrorName":   e.Error(),
+    "Environment": Environment,
+    "Request":     request,
+  }
 
-	if params["Class"] == "" {
-		params["Class"] = "Panic"
-	}
+  if params["Class"] == "" {
+    params["Class"] = "Panic"
+  }
 
-	pwd, err := os.Getwd()
-	if err == nil {
-		params["Pwd"] = pwd
-	}
+  pwd, err := os.Getwd()
+  if err == nil {
+    params["Pwd"] = pwd
+  }
 
-	params["Backtrace"] = stacktrace(3)
+  params["Backtrace"] = stacktrace(3)
 
-	post(params)
+  post(params)
 
-	return nil
+  return nil
 }
 
 func Notify(e error) error {
-	once.Do(initChannel)
+  once.Do(initChannel)
 
-	if ApiKey == "" {
-		return apiKeyMissing
-	}
+  if ApiKey == "" {
+    return apiKeyMissing
+  }
 
-	params := map[string]interface{}{
-		"Class":       reflect.TypeOf(e).String(),
-		"Error":       e,
-		"ApiKey":      ApiKey,
-		"ErrorName":   e.Error(),
-		"Environment": Environment,
-	}
+  params := map[string]interface{}{
+    "Class":       reflect.TypeOf(e).String(),
+    "Error":       e,
+    "ApiKey":      ApiKey,
+    "ErrorName":   e.Error(),
+    "Environment": Environment,
+  }
 
-	if params["Class"] == "" {
-		params["Class"] = "Panic"
-	}
+  if params["Class"] == "" {
+    params["Class"] = "Panic"
+  }
 
-	pwd, err := os.Getwd()
+  pwd, err := os.Getwd()
 
-	if err == nil {
-		params["Pwd"] = pwd
-	}
+  if err == nil {
+    params["Pwd"] = pwd
+  }
 
-	hostname, err := os.Hostname()
+  hostname, err := os.Hostname()
 
-	if err == nil {
-		params["Hostname"] = hostname
-	}
+  if err == nil {
+    params["Hostname"] = hostname
+  }
 
-	params["Backtrace"] = stacktrace(3)
+  params["Backtrace"] = stacktrace(3)
 
-	post(params)
-	return nil
+  post(params)
+  return nil
 
 }
 
 func CapturePanic(r *http.Request) {
-	if rec := recover(); rec != nil {
+  if rec := recover(); rec != nil {
 
-		if err, ok := rec.(error); ok {
-			log.Printf("Recording err %s", err)
-			Error(err, r)
-		} else if err, ok := rec.(string); ok {
-			log.Printf("Recording string %s", err)
-			Error(errors.New(err), r)
-		}
+    if err, ok := rec.(error); ok {
+      log.Printf("Recording err %s", err)
+      Error(err, r)
+    } else if err, ok := rec.(string); ok {
+      log.Printf("Recording string %s", err)
+      Error(errors.New(err), r)
+    }
 
-		panic(rec)
-	}
+    panic(rec)
+  }
 }
 
 const source = `<?xml version="1.0" encoding="UTF-8"?>
